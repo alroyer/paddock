855c783a99b86aa0957a4dd44d61ee8c26fe45c9
Alain Royer <royer_alain@hotmail.com>
2023-08-21 22:41:43 -0400

    added some packets

--- src/packet.py
+++ src/packet.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+CAR_MOTION_DATA_COUNT = 22
+
 
 @dataclass
 class Header:
@@ -16,3 +18,37 @@ class Header:
     player_car_index: int
     secondary_player_car_index: int
     secondary_player_car_index: int
+
+
+@dataclass
+class CarMotionData:
+    world_position_x: float
+    world_position_y: float
+    world_position_z: float
+    world_velocity_x: float
+    world_velocity_y: float
+    world_velocity_z: float
+    world_forward_dir_x: int
+    world_forward_dir_y: int
+    world_forward_dir_z: int
+    world_right_dir_x: int
+    world_right_dir_y: int
+    world_right_dir_z: int
+    g_force_lateral: float
+    g_force_longitudinal: float
+    g_force_vertical: float
+    yaw: float
+    pitch: float
+    roll: float
+
+
+@dataclass
+class MotionData:
+    header: Header
+    car_motion_data: list[CarMotionData]
+
+
+@dataclass
+class MarshalZone:
+    zone_start: float
+    zone_flag: int
