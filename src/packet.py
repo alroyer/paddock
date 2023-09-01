edbf3b16c0217c2e1d718d23b856e195475b858a
Alain Royer <royer_alain@hotmail.com>
2023-09-01 19:43:06 -0400

    parsing (wip)

--- src/packet.py
+++ src/packet.py
@@ -2,24 +2,33 @@ import struct
 
 from packet.event import button_status_to_buttons
 from packet.header import PacketHeader
+from packet.parser import parse
 
 
 def main():
     with open('./data/telemetry.bin', 'rb') as file:
         data = file.read()
-        print(len(data))
 
-        header = PacketHeader(*struct.unpack('<HBBBBBQfIIBB', data[:29]))
-        print(header)
+        packets = parse(data)
+        print(packets)
 
-        event_string_code = ''.join([chr(b) for b in struct.unpack('BBBB', data[29:33])])
-        print(event_string_code)
+    # with open('./data/telemetry.bin', 'rb') as file:
+    #     data = file.read()
+    #     print(len(data))
 
-        button_status = struct.unpack('I', data[33:37])[0]
-        print(button_status)
+    #     header = PacketHeader(*struct.unpack('<HBBBBBQfIIBB', data[:29]))
+    #     print(header)
 
-        buttons = button_status_to_buttons(button_status)
-        print(buttons)
+    #     event_string_code = ''.join(
+    #         [chr(b) for b in struct.unpack('BBBB', data[29:33])]
+    #     )
+    #     print(event_string_code)
+
+    #     button_status = struct.unpack('I', data[33:37])[0]
+    #     print(button_status)
+
+    #     buttons = button_status_to_buttons(button_status)
+    #     print(buttons)
 
 
 if __name__ == '__main__':
--- src/packet/parser.py
+++ src/packet/parser.py
@@ -1,4 +1,5 @@
-from typing import Union
+import struct
+from typing import Tuple, Union
 
 from .event import PacketEventData
 from .header import PacketHeader
@@ -7,6 +8,41 @@ from .motion import PacketMotionData
 from .session import PacketSessionData
 
 
-def parse(data: bytes) -> Union[PacketEventData, PacketLapData, PacketMotionData, PacketSessionData]:
-    # TODO
+class InvalidData(Exception):
     pass
+
+
+def _parse(
+    data: bytes,
+) -> Tuple[
+    Union[PacketEventData, PacketLapData, PacketMotionData, PacketSessionData], bytes
+]:
+    header = PacketHeader(*struct.unpack('<HBBBBBQfIIBB', data[:29]))
+    if header.packet_id == 3:
+        packet_event, data = _parse_event(header, data[29:])
+        return packet_event, data
+    raise InvalidData()
+
+
+def _parse_event(header: PacketHeader, data: bytes) -> Tuple[PacketEventData, bytes]:
+    event_string_code = ''.join([chr(b) for b in struct.unpack('BBBB', data[:4])])
+    if event_string_code == 'BUTN':
+        buttons_event, data = _parse_buttons_event(header, data[5:])
+        return buttons_event, data
+    raise InvalidData()
+
+
+def _parse_buttons_event(
+    header: PacketHeader, data: bytes
+) -> Tuple[PacketEventData, bytes]:
+    pass
+
+
+def parse(
+    data: bytes,
+) -> list[Union[PacketEventData, PacketLapData, PacketMotionData, PacketSessionData]]:
+    packets = []
+    while data:
+        packet, data = _parse(data)
+        packets.append(packet)
+    return packets
